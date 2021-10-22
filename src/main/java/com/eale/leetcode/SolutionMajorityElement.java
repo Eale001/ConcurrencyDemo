886d6fc712a254e04d701b46efc451cea1e25696
hezhihui <hezhihui>
2021-10-22 16:55:00 +0800

    2021-10-22 			hzh 			leetcode -【229】求众数II

--- src/main/java/com/eale/leetcode/SolutionMajorityElement.java
+++ src/main/java/com/eale/leetcode/SolutionMajorityElement.java
@@ -1,5 +1,7 @@
 package com.eale.leetcode;
 
+import java.util.*;
+
 /**
  * 【229】求众数II
  *
@@ -29,9 +31,36 @@ public class SolutionMajorityElement {
      *  -109 <= nums[i] <= 109
      *
      *  进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1)的算法解决此问题。
-     *  Related Topics 数组 哈希表 计数 排序
+     *
+     *  Related Topics 数组 哈希表 计数 排序  摩尔投票法
      *  👍 486 👎 0
      */
+
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int i = nums.length/3;
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            Integer total = map.get(num);
+            if (Objects.isNull(total)){
+                total = 1;
+            }else {
+                total ++;
+            }
+            map.put(num,total);
+            if (total > i && !result.contains(num)){
+                result.add(num);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{3,2,3}));
+        System.out.println(majorityElement(new int[]{1}));
+        System.out.println(majorityElement(new int[]{1,1,1,3,3,2,2,2}));
+
+    }
 
 
 }
